b4fdb2c73926b1a405b2b42b2797800fc155adf6
jonariv09 <ingjoseagustinnarvaez@gmail.com>
2020-02-07 17:15:57 -0600

    NavBar style finished and some bootstrap styles overwritten

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import '../css/App.css'
+import NavBar from './NavBar'
+
 
 export default function App() {
   return (
@@ -8,7 +10,7 @@ export default function App() {
         <div className="content-wrapper d-flex flex-column">
           <div className={"content"}>
 
-
+              <NavBar />
 
           </div>
         </div>
