4865dd02badf46225bb16b65a359abbe0a1ef5cd
jonariv09 <ingjoseagustinnarvaez@gmail.com>
2020-02-06 20:28:03 -0600

    Creating UserPost and NavBar

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import '../css/App.css'
 
 export default function App() {
   return (
     <>
       <div className={"wrapper"}>
-        <div className="content-wrapper">
+        <div className="content-wrapper d-flex flex-column">
           <div className={"content"}>
 
 
