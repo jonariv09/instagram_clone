699aa759a39f27cf2c2c4403d820c5c3690d1968
jonariv09 <ingjoseagustinnarvaez@gmail.com>
2020-02-06 19:46:17 -0600

    Starting main Layout

---
+++ frontend/src/Components/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+export default function App() {
+  return (
+    <>
+      <div className={"wrapper"}>
+        <div className="content-wrapper">
+          <div className={"content"}>
+
+            
+
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
