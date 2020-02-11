400737bdeba688d1ae0d57f783023c3e601523c1
jonariv09 <ingjoseagustinnarvaez@gmail.com>
2020-02-11 12:41:15 -0600

    Adding images and other files

---
+++ frontend/src/Components/States.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class States extends Component {
+  constructor (props) {
+    super(props);
+  }
+
+  render () {
+    return (
+      <>
+
+
+      </>
+    )
+  }
+
+}
