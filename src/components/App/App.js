54561acb8f6228b19a450b1a6de507e7542e22b6
jonariv09 <ingjoseagustinnarvaez@gmail.com>
2020-04-08 17:42:14 -0600

    Style structure of sign-in form done ✔️

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,4 @@
 import React from 'react'
-import './../../css/Overwrite.css'
-import './../../css/GeneralStyles.css'
 import AppRoutes from '../../routes'
 
 export default function App() {
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import SignIn from '../../components/SignIn/index'
+import Phone from './../../assets/images/phone.png'
 
 function Login() {
   return (
     <>
-      <SignIn />
+      <div className={'wrapper'}>
+        <div className="content-wrapper d-flex flex-column">
+          <div className={'content'}>
+            <div className="row d-flex justify-content-center pt-4">
+              <div className="col-md-6 d-flex justify-content-end p-0">
+                <img src={Phone} alt="" />
+              </div>
+              <div className="col-md-6 d-flex justify-content-start p-0">
+                <SignIn />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
